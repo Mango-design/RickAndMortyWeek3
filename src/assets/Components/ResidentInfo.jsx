7935455265d2823ab44dfd643c3a739d20209213
Mango-design <angelclash21@gmail.com>
2023-01-07 16:28:49 -0600

    version

--- src/assets/Components/ResidentInfo.jsx
+++ src/assets/Components/ResidentInfo.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const ResidentInfo = () => {
+const ResidentInfo = ({resident}) => {
     return (
         <div>
             <h1>Hola</h1>
-           
-            
+            <h1>{resident[4]}</h1>
         </div>
     );
 };
