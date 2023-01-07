6cd29cb61a0b5dca618233864b29a939c49e7ea4
Mango-design <angelclash21@gmail.com>
2023-01-07 13:15:32 -0600

    version bugeada

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,28 @@ import reactLogo from './assets/react.svg'
 import Banner from './assets/components/banner'
 import axios from 'axios'
 import './App.css'
-import ChangeLocation from './assets/Components/ChangeLocation'
-
 
 function App() {
 
   const [dimensionType, setDimensionType] = useState({})
-  const id = Math.floor(Math.random() * 126) + 1
-  const [random, setRandom] = useState(id)
 
 
 
-  
+
   useEffect(() => {
-           
-   axios.get(`https://rickandmortyapi.com/api/location/${random}`)
-   .then((res) => setDimensionType(res.data));
-  }, [random]);
- 
- 
+    const randomId = Math.floor(Math.random() * 126) + 1;
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${randomId}`)
+      .then((res) => setDimensionType(res.data));
+  }, []);
 
   return (
     <div className="App">
+     <Banner/>
      <h2>{dimensionType.name}</h2>
      <h2>{dimensionType.type}</h2>
      <h2>{dimensionType.dimension}</h2>
      <h2>{dimensionType.residents?.length}</h2>
-     < ChangeLocation random={random} setRandom={setRandom}/>
-     
-     
-     <Banner/>
-    
     </div>
     
   )
