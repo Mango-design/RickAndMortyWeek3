b8521eb972ae19cf69c2fabdb316072d9c666e13
CarolinaVlr <dianacarolinav97@gmail.com>
2023-01-09 14:17:21 -0500

    style css

--- src/assets/Components/ChangeLocation.jsx
+++ src/assets/Components/ChangeLocation.jsx
@@ -4,7 +4,7 @@ const ChangeLocation = ({random, setRandom}) => {
     const [location, setLocation] = useState("")
     return (
         <div className='changeLocation'>
-            <input type="text" className='input' value={location} onChange={e=>setLocation(e.target.value)} placeholder='Type location Id' />
+            <input type="text" className='input' value={location} onChange={e=>setLocation(e.target.value)} placeholder='Type location Id' onKeyDown={(e) => {e.keyCode === 13 ? setRandom(location) : null}}/>
             <button onClick={()=>{setRandom(location)}} >Search</button>
             
             
--- src/assets/Components/ResidentInfo.jsx
+++ src/assets/Components/ResidentInfo.jsx
@@ -2,29 +2,46 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 
-const ResidentInfo = ({resident}) => {
+const ResidentInfo = ({ resident }) => {
     const [person, setPerson] = useState({})
+
+
+
     useEffect(() => {
         axios.get(`${resident}`)
-        .then(res => setPerson(res.data))
-     ,[]})
+            .then(res => setPerson(res.data))
+
+    }, [])
+    let color = "";
+    switch (person.status) {
+        case 'Dead':
+            color = 'red';
+            break;
+        case 'Alive':
+            color = 'green';
+            break;
+        default:
+            color = 'gray';
+    }
     return (
         <div className='residentInfo'>
-              <div>
-             <img src={person.image} alt="" />
-             <div>
-                 <h2>{person.name}</h2>
-                 <h2>{person.status} - {person.species}</h2>
-             
-            
-                 <h2>{person.origin?.name}</ h2>
+            <div className='residentInfo_card'>
+                <img src={person.image} alt="" />
+                <div>
+                    <h2>{person.name}</h2>
+                    <h2 className='status'>
+                        <i class="fa-solid fa-circle" style={{ color: color }}></i> {person.status} - {person.species}</h2>
 
-                 <h2>Episodes where appear</h2>
-                 <h2>{person.episode?.length}</h2>
-           
-            
 
-            </div>
+
+                    <h2><span>Origen</span> {person.origin?.name}</ h2>
+
+
+                    <h2><span>Apariciones</span> {person.episode?.length}</h2>
+
+
+
+                </div>
 
             </div>
         </div>
